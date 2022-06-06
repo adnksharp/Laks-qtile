04e403e0f9a7ff5b6755de4ab5ada790b59eaa36
adnKsharp <83608645+ADN227@users.noreply.github.com>
2022-06-06 02:12:58 -0600

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -13,5 +13,5 @@ feh --bg-fill ~/Imagenes/108.jpeg
 # Desactivar el apagado de pantalla por inactividad
 xset s off s noblank -dpms
 
-# Autoconfigurar las pantallas
-xrandr --output HDMI1 --auto --output LVDS1 --auto --output VGA1 --auto
+# Autoconfigurar las pantallas alineadas horizontalmente
+xrandr --output LVDS1 --auto --primary --output HDMI1 --auto --pos $(xrandr | grep 'primary' | awk '{print $4}' | awk -F 'x' '{print $1}')x0
